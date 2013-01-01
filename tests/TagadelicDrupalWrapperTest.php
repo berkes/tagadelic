4e0080e6ed574d8d59bb0eeda79efd6b2076f4a2
Bèr Kessels <ber@webschuur.com>
2013-01-01 17:08:11 +0100

    Move Fake Drupal into test-file.
    * File was only included from there.
    * Extra file caused coverage-report to break.

--- tests/TagadelicDrupalWrapperTest.php
+++ tests/TagadelicDrupalWrapperTest.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once 'TagadelicDrupalWrapper.php';
-require_once 'TagadelicFakeDrupal.php';
-
 class TagadelicDrupalWrapperTest extends PHPUnit_Framework_TestCase {
   /**
    * @var TagadelicDrupalWrapper
@@ -100,3 +98,31 @@ class TagadelicDrupalWrapperTest extends PHPUnit_Framework_TestCase {
     $this->object->l("text");
   }
 }
+
+/**
+ * Fake Drupal stubs the global functions
+ */
+
+function cache_get() {
+  return __sig("cache_get", func_get_args());
+}
+function cache_set() {
+  return __sig("cache_set", func_get_args());
+}
+function check_plain() {
+  return __sig("check_plain", func_get_args());
+}
+function l() {
+  return __sig("l", func_get_args());
+}
+
+function __sig($func, $args) {
+  foreach($args as $id => $arg) {
+    if (is_array($arg) && !empty($arg)) {
+      $args[$id] = serialize($arg);
+    }
+  }
+  $arglist = join(',', $args);
+  return "{$func}({$arglist})";
+}
+
