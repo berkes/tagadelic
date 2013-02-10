301881b3aaacaad5bb00977394b207d85e690e9f
Bèr Kessels <ber@webschuur.com>
2013-02-10 16:19:18 +0100

    Drupal release mgmt requires master to be blank

--- README.txt
+++
@@ -1,2 +0,0 @@
-The master branch is intentionally empty. Please check out the branch which
-corresponds to your Drupal version (e.g. '7.x-1.x').
--- templates/tagadelic_taxonomy_cloud.tpl.php
+++
@@ -1,7 +0,0 @@
-<ul class="tag-cloud">
-  <?php foreach ($tags as $tag): ?>
-  <li>
-    <?php print $tag ?>
-  </li>
-  <?php endforeach; ?>
-</ul>
